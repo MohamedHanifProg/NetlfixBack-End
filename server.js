91fa3d9b3339cffd5ece829b287e442c4abd678f
MohamedHnef <mohamed.hnef1@gmail.com>
2025-04-12 19:49:51 +0300

    mongo connection setup

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-
-import authRoutes from './routes/auth.js'; // you'll create this next
-
-dotenv.config();
+// server.js
+const express = require('express');
+const connectDB = require('./db_connection');
 
 const app = express();
-app.use(cors());
+
+
+connectDB();
+
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+});
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
