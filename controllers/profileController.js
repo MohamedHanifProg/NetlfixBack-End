4ad0d024fac25e4e93ae2e2858dce59b6ae6e775
MohamedHnef <mohamed.hnef1@gmail.com>
2025-04-13 15:00:17 +0300

    added add/delete/edit profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -41,4 +41,53 @@ const updateProfile = async (req, res, next) => {
   }
 };
 
-module.exports = { getProfiles, updateProfile };
+const addProfile = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { profileName, avatar } = req.body;
+    
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    // Enforce maximum 5 profiles on the backend as well
+    if (user.profiles.length >= 5) {
+      return res.status(400).json({ message: "Maximum number of profiles reached" });
+    }
+    
+    // Create new profile subdocument (Mongoose will automatically assign an _id)
+    const newProfile = { profileName, avatar };
+    user.profiles.push(newProfile);
+    await user.save();
+
+    // Return the newly created profile (the last one in the array)
+    res.status(201).json({ profile: user.profiles[user.profiles.length - 1] });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteProfile = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const profileId = req.params.profileId;
+    
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    // Remove the profile subdocument using the pull method
+    user.profiles.pull(profileId);
+    await user.save();
+    
+    res.status(200).json({ message: "Profile deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = { getProfiles, updateProfile, addProfile , deleteProfile};
+
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,10 +1,12 @@
 // routes/profileRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getProfiles, updateProfile } = require('../controllers/profileController');
+const { getProfiles, updateProfile, addProfile , deleteProfile } = require('../controllers/profileController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.get('/profiles', protect, getProfiles);
 router.put('/profiles/:profileId', protect, updateProfile);
+router.post('/profiles', protect, addProfile); 
+router.delete('/profiles/:profileId', protect, deleteProfile);
 
 module.exports = router;
