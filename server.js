6aee9ded0da537a72f965b2cf8d01464326d0062
MohamedHnef <mohamed.hnef1@gmail.com>
2025-04-18 03:20:49 +0300

    deploy

--- server.js
+++ server.js
@@ -62,3 +62,7 @@ app.use((err, req, res, next) => {
 // ────────────── start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
